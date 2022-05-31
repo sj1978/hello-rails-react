4e990dbaf48b7d5a7922dcc15f55b228c063e4ce
Anibal Amoroso <anibaldaniel2212@gmail.com>
2022-05-31 09:53:31 -0300

    add rails files

---
+++ app/controllers/v1/messages_controller.rb
@@ -0,0 +1,7 @@
+
+class V1::MessagesController < ApplicationController
+    def index
+      random_msg = Message.find(Message.pluck(:id).sample)
+      render json: { message: random_msg }.to_json
+    end
+  end
